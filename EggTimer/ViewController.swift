faeed73349fe40c76e0c8125eb069d662a7b8a93
GokulRG <ramakrishnan.go@husky.neu.edu>
2021-01-04 08:10:41 -0800

    finishing the egg timer project and adding alarm sound after timer completion

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var player: AVAudioPlayer?
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        self.progressBar.progress = 0
+        let minutes = eggTimes[sender.currentTitle!]!
+        label.text = sender.currentTitle!
+        let totalTime = minutes * 60
+        var timePassed = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            timePassed += 1
+            self.progressBar.progress = Float(timePassed)/Float(totalTime)
+            if(totalTime - timePassed == 0){
+                timer.invalidate()
+                self.label.text = "Done!"
+                self.playSound()
+            }
+        }
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
